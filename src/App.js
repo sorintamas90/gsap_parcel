3efa1096cd6f8a2602c6edb5bd58888fb574894e
Sorin <tamas.sorin90@gmail.com>
2024-05-17 09:38:00 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import \n gsap } from "gsap";
+
+export const App = () => \n
+  gsap.fromTo(
+    "#h1",
+    \n color: "red" },
+    \n color: "blue", duration: 2 }
+  );
+
+  return <h1 id="h1">Hello world 12343</h1>;
+};
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import \n createRoot } from "react-dom/client";
+import \n App } from "./App";
+
+const container = document.getElementById("app");
+const root = createRoot(container)
+root.render(<App />);
