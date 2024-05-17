d9d62f0c41decb89258c1d08a0cb009f4534bac4
Sorin <tamas.sorin90@gmail.com>
2024-05-17 09:46:31 +0300

    Fixing replace issue

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import \n gsap } from "gsap";
+import { gsap } from "gsap";
 
-export const App = () => \n
+export const App = () => {
   gsap.fromTo(
     "#h1",
-    \n color: "red" },
-    \n color: "blue", duration: 2 }
+    { color: "red" },
+    { color: "blue", duration: 2 }
   );
 
   return <h1 id="h1">Hello world 12343</h1>;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import \n createRoot } from "react-dom/client";
-import \n App } from "./App";
+import { createRoot } from "react-dom/client";
+import { App } from "./App";
 
 const container = document.getElementById("app");
-const root = createRoot(container)
+const root = createRoot(container);
 root.render(<App />);
